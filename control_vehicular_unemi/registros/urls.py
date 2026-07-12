c5b94be2369ef008032cc18c569246066a91df31
Darix-io <rmoraj3@unemi.edu.ec>
2026-07-11 22:07:12 -0500

    implementacion del dashboard, escaneo de QR

--- control_vehicular_unemi/registros/urls.py
+++ control_vehicular_unemi/registros/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('validar/', views.validar_acceso, name='validar_acceso'),
+    path('dashboard/', views.dashboard_guardia, name='dashboard_guardia'), # <--- Agrega esta línea
 ]
--- control_vehicular_unemi/registros/views.py
+++ control_vehicular_unemi/registros/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required # <-- Agrega esta línea
 from accesos.models import Vehiculo
 from .models import RegistroAcceso, TipoAcceso
 
+@login_required(login_url='/admin/login/')
 def validar_acceso(request):
     resultado = None
     if request.method == 'POST':
@@ -16,3 +18,19 @@ def validar_acceso(request):
             resultado = "Error: Vehículo no encontrado en la base de datos."
             
     return render(request, 'registros/validar.html', {'resultado': resultado})
+
+@login_required(login_url='/admin/login/')
+def dashboard_guardia(request):
+    # Obtener los últimos 10 accesos registrados en el sistema
+    ultimos_accesos = RegistroAcceso.objects.select_related('vehiculo').order_by('-fecha_hora')[:10]
+    
+    # Cálculo rápido para el contador del campus
+    total_entradas = RegistroAcceso.objects.filter(tipo='Entrada').count()
+    total_salidas = RegistroAcceso.objects.filter(tipo='Salida').count()
+    vehiculos_dentro = max(0, total_entradas - total_salidas)
+    
+    context = {
+        'ultimos_accesos': ultimos_accesos,
+        'vehiculos_dentro': vehiculos_dentro,
+    }
+    return render(request, 'registros/dashboard.html', context)
